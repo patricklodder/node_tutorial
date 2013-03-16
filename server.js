e67af2cd436574315c597ea531c20a386dcb796c
Gacnt <gacnt@shaw.ca>
2013-03-15 21:18:22 -0600

    You've sent the text: undefined

--- server.js
+++ server.js
@@ -2,9 +2,21 @@ var http = require("http");
 var url = require("url");
 
 function start(route, handle) {
-	function onRequest(request, repsonse) {
+	function onRequest(request, response) {
+		var postData = "";
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " receieved.");
+
+		request.setEncoding("utf8");
+		request.addListener("data", function(postDataChunk) {
+			postData += postDataChunk;
+			console.log("Received POST data chunk '" + 
+				postDataChunk + "'.");
+		});
+
+		request.addListener("end", function() {
+			route(handle, pathname, response, postData);
+		});
 
 		route(handle, pathname, response);
 	}
